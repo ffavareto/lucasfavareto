975cbe547551910b1b34cdd1a6a57477bfd65855
Lucas Santos <lucasfavaretosantos98@gmail.com>
2024-04-05 17:26:36 -0300

    aria-label e title em links

--- src/app/components/profile.js
+++ src/app/components/profile.js
@@ -25,34 +25,58 @@ export default function Profile() {
           className="rounded-full mb-4"
         />
 
-        <h1 className="text-slate-100 text-4xl font-mono">Lucas Santos</h1>
-        <h1 className="text-slate-100 font-mono">Front-End Developer</h1>
+        <div>
+          <h1 className="text-slate-100 text-4xl font-mono text-center">
+            Lucas Santos
+          </h1>
+          <h1 className="text-slate-100 font-mono text-center">
+            Front-End Developer
+          </h1>
+        </div>
 
         <div className="flex gap-4 mt-4">
-          <a href="https://github.com/ffavareto" target="_blank">
+          <a
+            href="https://github.com/ffavareto"
+            target="_blank"
+            aria-label="GitHub"
+          >
             <FontAwesomeIcon
               icon={faGithub}
               color="#f1f5f9"
               height={42}
               width={42}
+              title="GitHub"
+              aria-label="GitHub"
             />
           </a>
 
-          <a href="https://www.linkedin.com/in/lucasfavareto/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/lucasfavareto/"
+            target="_blank"
+            aria-label="LinkedIn"
+          >
             <FontAwesomeIcon
               icon={faLinkedin}
               color="#f1f5f9"
               height={42}
               width={42}
+              title="LinkedIn"
+              aria-label="LinkedIn"
             />
           </a>
 
-          <a href="mailto:lucasfavaretosantos98@gmail.com" target="_blank">
+          <a
+            href="mailto:lucasfavaretosantos98@gmail.com"
+            target="_blank"
+            aria-label="Gmail"
+          >
             <FontAwesomeIcon
               icon={faGoogle}
               color="#f1f5f9"
               height={42}
               width={42}
+              title="Gmail"
+              aria-label="Gmail"
             />
           </a>
         </div>
