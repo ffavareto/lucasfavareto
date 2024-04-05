630035ec09bc361ac40d822b844babec74d2c15a
Lucas Santos <lucasfavaretosantos98@gmail.com>
2024-04-05 16:51:14 -0300

    responsividade do currículo

--- src/app/curriculum/page.js
+++ src/app/curriculum/page.js
@@ -6,54 +6,56 @@ export default function Curriculum() {
 
   return (
     <>
-      <div>
+      <div className="p-4">
         <div className="text-slate-100">
           <div className="m-8">
             <h2 className="text-center font-mono">CURRÍCULO</h2>
           </div>
 
           <div className="mt-8 flex items-center justify-center">
-            <h1 className="w-fit text-center text-5xl border-4 border-slate-100 p-5 pb-3 font-bold font-mono">
+            <h1 className="w-fit text-center text-2xl md:text-5xl border-4 border-slate-100 p-5 pb-3 font-bold font-mono">
               LUCAS SANTOS
             </h1>
           </div>
 
           <div className="mb-2 flex items-center justify-center text-slate-400 flex-col">
-            <h1 className="w-fit text-center text-2xl p-5 pb-3 font-bold font-mono">
+            <h1 className="w-fit text-center md:text-2xl p-5 pb-3 font-bold font-mono">
               DESENVOLVEDOR FRONT-END
             </h1>
           </div>
         </div>
 
-        <div className="p-4">
+        <div>
           <div className="border-2 border-slate-100 p-5 font-mono flex flex-col gap-2 text-slate-100">
             <Title>Sobre mim</Title>
-            <p>
-              Sou um desenvolvedor front-end com especialização em tecnologias
-              como Angular e TypeScript, acumulando mais de {experienceYears}{" "}
-              anos de experiência desde o meu início como estagiário em novembro
-              de 2019.
-            </p>
-            <p>
-              Tenho uma boa base técnica no front-end e estou constantemente
-              ampliando meu conhecimento, recentemente explorando áreas como
-              SQL, NestJS e Docker. Além disso, tenho me dedicado ao
-              aprimoramento em testes unitários, reconhecendo sua importância
-              fundamental para garantir a qualidade e estabilidade de qualquer
-              aplicação em produção.
-            </p>
-            <p>
-              Minha experiência abrange desde novas implementações até
-              manutenções em projetos existentes, incluindo migrações de
-              tecnologia. Estou comprometido em buscar sempre os melhores
-              padrões e práticas de desenvolvimento para entregar soluções de
-              alta qualidade e desempenho.
-            </p>
+            <div className="flex flex-col gap-4 text-xs md:text-base">
+              <p>
+                Sou um desenvolvedor front-end com especialização em tecnologias
+                como Angular e TypeScript, acumulando mais de {experienceYears}{" "}
+                anos de experiência desde o meu início como estagiário em
+                novembro de 2019.
+              </p>
+              <p>
+                Tenho uma boa base técnica no front-end e estou constantemente
+                ampliando meu conhecimento, recentemente explorando áreas como
+                SQL, NestJS e Docker. Além disso, tenho me dedicado ao
+                aprimoramento em testes unitários, reconhecendo sua importância
+                fundamental para garantir a qualidade e estabilidade de qualquer
+                aplicação em produção.
+              </p>
+              <p>
+                Minha experiência abrange desde novas implementações até
+                manutenções em projetos existentes, incluindo migrações de
+                tecnologia. Estou comprometido em buscar sempre os melhores
+                padrões e práticas de desenvolvimento para entregar soluções de
+                alta qualidade e desempenho.
+              </p>
+            </div>
           </div>
 
           <div className="border-2 border-slate-100 p-5 font-mono flex flex-col gap-2 mt-4 text-slate-100">
             <Title>Experiência</Title>
-            <div>
+            <div className="flex flex-col gap-4 text-xs md:text-base">
               <div className="mb-4">
                 <h2>ECS Inovação - Desenvolvedor front-end</h2>
                 <p>Setembro de 2022 - presente</p>
