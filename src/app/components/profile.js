57204bf3afa9a384f01de968a707f3ef88ff806f
Lucas Santos <lucasfavaretosantos98@gmail.com>
2024-04-05 17:11:23 -0300

    uso do fontawesome

--- src/app/components/profile.js
+++ src/app/components/profile.js
@@ -5,6 +5,13 @@ import linkedin from "../../../public/linkedin.svg";
 import gmail from "../../../public/gmail.svg";
 import lucas from "../../../public/lucas-santos.jpeg";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faGoogle,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function Profile() {
   return (
     <>
@@ -21,34 +28,31 @@ export default function Profile() {
         <h1 className="text-slate-100 text-4xl font-mono">Lucas Santos</h1>
         <h1 className="text-slate-100 font-mono">Front-End Developer</h1>
 
-        <div className="flex gap-2 mt-4">
+        <div className="flex gap-4 mt-4">
           <a href="https://github.com/ffavareto" target="_blank">
-            <Image
-              src={github}
-              width={56}
-              height={56}
-              alt="GitHub logo"
-              priority={true}
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#f1f5f9"
+              height={42}
+              width={42}
             />
           </a>
 
           <a href="https://www.linkedin.com/in/lucasfavareto/" target="_blank">
-            <Image
-              src={linkedin}
-              width={56}
-              height={56}
-              alt="Linkedin logo"
-              priority={true}
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#f1f5f9"
+              height={42}
+              width={42}
             />
           </a>
 
           <a href="mailto:lucasfavaretosantos98@gmail.com" target="_blank">
-            <Image
-              src={gmail}
-              width={56}
-              height={56}
-              alt="Gmail logo"
-              priority={true}
+            <FontAwesomeIcon
+              icon={faGoogle}
+              color="#f1f5f9"
+              height={42}
+              width={42}
             />
           </a>
         </div>
