6e3e2f7b501ec1f6368394ebb797e4aa10834ac6
Lucas Santos <lucasfavaretosantos98@gmail.com>
2024-04-05 10:38:22 -0300

    pequeno ajuste no `tailwind.config.js`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/app/**/*.{js,jsx}", "./src/app/components/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
