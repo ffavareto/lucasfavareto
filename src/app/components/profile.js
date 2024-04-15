cdc31a310a04022d2ec7988d9cd32a0338c65186
Lucas Santos <lucasfavaretosantos98@gmail.com>
2024-04-15 16:12:28 -0300

    Angular Certified

--- src/app/components/profile.js
+++ src/app/components/profile.js
@@ -6,6 +6,7 @@ import {
   faGithub,
   faGoogle,
   faLinkedin,
+  faAngular,
 } from "@fortawesome/free-brands-svg-icons";
 
 export default function Profile() {
@@ -23,7 +24,16 @@ export default function Profile() {
 
         <div>
           <h1 className="text-slate-100 text-4xl  text-center">Lucas Santos</h1>
-          <h1 className="text-slate-100  text-center">Front-End Developer</h1>
+          <h1 className="text-slate-100  text-center">
+            Front-End Developer -&nbsp;
+            <a
+              href="https://interstate21.com/certificate/?code=5D14JTX"
+              target="_blank"
+              className="text-red-500 font-bold"
+            >
+              Angular Certified
+            </a>
+          </h1>
         </div>
 
         <div className="flex gap-4 mt-4">
@@ -68,6 +78,21 @@ export default function Profile() {
               height={42}
               icon={faGoogle}
               title="Gmail"
+              width={42}
+            />
+          </a>
+
+          <a
+            aria-label="Angular Certified"
+            href="https://interstate21.com/certificate/?code=5D14JTX"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              aria-label="Angular Certified"
+              color="#f1f5f9"
+              height={42}
+              icon={faAngular}
+              title="Angular Certified"
               width={42}
             />
           </a>
